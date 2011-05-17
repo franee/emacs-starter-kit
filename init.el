2a77dc56962db1ccc8dba7e55e8c3cb7a120a208
franee <franee@caresharing.eu>
2011-05-17 17:18:38 +0800

    franee customizations

--- init.el
+++ init.el
@@ -77,3 +77,44 @@
   (mapc #'load (directory-files user-specific-dir nil ".*el$")))
 
 ;;; init.el ends here
+
+
+;;;### FRANEE CUSTOMIZATIONS ###
+
+;; set default dir
+(setq default-directory "/opt/palmade/development/")
+
+;; show line numbers
+(global-linum-mode 1)
+
+;; git
+(require 'magit)
+
+;; remove trailing spaces on save
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Kills live buffers, leaves some emacs work buffers
+;; optained from http://www.chrislott.org/geek/emacs/dotemacs.html
+(defun nuke-some-buffers (&optional list)
+  "For each buffer in LIST, kill it silently if unmodified. Otherwise ask.
+LIST defaults to all existing live buffers."
+  (interactive)
+  (if (null list)
+      (setq list (buffer-list)))
+  (while list
+    (let* ((buffer (car list))
+           (name (buffer-name buffer)))
+      (and (not (string-equal name ""))
+           (not (string-equal name "*Messages*"))
+           (not (string-equal name "*Buffer List*"))
+           (not (string-equal name "*buffer-selection*"))
+           (not (string-equal name "*Shell Command Output*"))
+           (not (string-equal name "*scratch*"))
+           (/= (aref name 0) ? )
+           (if (buffer-modified-p buffer)
+               (if (yes-or-no-p
+                    (format "Buffer %s has been edited. Kill? " name))
+                   (kill-buffer buffer))
+             (kill-buffer buffer))))
+    (setq list (cdr list))))
+
